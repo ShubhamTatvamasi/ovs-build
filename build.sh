2f925b8b1ed7c487ce0d5df4312a72947f2dc4e8
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-09-05 10:24:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+set -ex
+
 git clone https://github.com/openvswitch/ovs.git --depth 1
+
+# git clone https://github.com/openvswitch/ovs.git
+# git fetch
+# git checkout v2.15.1
 
 cd ovs
 
-./boot.sh
+# ./boot.sh
 
-./configure
+# ./configure
 
-make
+sudo DEB_BUILD_OPTIONS="parallel=$(nproc) nocheck" fakeroot debian/rules binary
 
-make dist
+#  --with-linux=/lib/modules/$(uname -r)/build
+
+# fakeroot make
+# make
+
+# make dist
 
